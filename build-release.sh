5499ccfd875b01d0629165a59318a58ef2e3b411
Dananjaya Perera <dananjaya01@gmail.com>
2018-12-29 01:41:44 +0530

    fixes and added build scripts

---
+++ build-release.sh
@@ -0,0 +1,49 @@
+
+mode="android";
+if [ -z "$1" ]
+then
+    mode="android";
+else
+    mode=$1;
+fi
+
+if [ "$mode" = "android" ]
+then
+    #rel='platforms/android/app/build/outputs/apk/release'
+    #relfile='app-release-unsigned.apk'
+    rel='platforms/android/build/outputs/apk/release'
+    relfile='android-release-unsigned.apk';
+
+    rm $rel/$env-app.apk
+    rm $rel/android-release-unsigned.apk
+    #rm platforms/* -rf
+    #ionic cordova platform add android@6.4.0
+fi
+
+echo -e "\e[44m\e[1;97m   building - $env [$mode]   \e[0m"
+
+cordova clean $mode
+ionic cordova build $mode --prod --release --verbose --target=production --minifycss --minifyjs --optimizejs
+
+if [ "$mode" = "android" ]
+then
+
+    #keytool -genkey -v -keystore release-key.keystore -alias dzero -keyalg RSA -keysize 2048 -validity 10000 -deststoretype pkcs12
+
+    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release-key.keystore -storetype pkcs12 -storepass Fw85HBcN -keypass Fw85HBcN -tsa http://timestamp.digicert.com $rel/$relfile dzero
+
+    zipalign -v 4 $rel/$relfile $rel/$env-app.apk
+
+    echo $rel/$env-app.apk
+
+    #keytool -exportcert -keystore release-key.keystore -list -v -alias dzero
+
+    #if [ -z "$2" ] then
+    #else
+        #adb install $rel/$env-app.apk
+    #if
+
+    #cordova plugin add cordova-plugin-crosswalk-webview --variable XWALK_MODE="embedded" --variable XWALK_MULTIPLEAPK="false" --variable XWALK_VERSION="23+" --variable XWALK_LITEVERSION="xwalk_core_library_canary:17+" --variable XWALK_COMMANDLINE="--disable-pull-to-refresh-effect"
+#else
+    #export APP_ENV=$env && ionic cordova build $mode --release --$env --verbose --target=production
+fi
--- src/pages/profile-view/profile-view.ts
+++ src/pages/profile-view/profile-view.ts
@@ -4,6 +4,7 @@ import { Api } from '../../providers';
 import { CallNumber } from '@ionic-native/call-number';
 import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../providers/user/user';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /**
  * Generated class for the ProfileViewPage page.
@@ -27,11 +28,13 @@ export class ProfileViewPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     private api:Api, private user:User, public translateService: TranslateService, 
+    public sanitizer: DomSanitizer,
     private callNumber: CallNumber) {
 
     if (navParams.get('item')) this.item = navParams.get('item');
     this.apiroot = api.url;
     this._user = this.user.USER;
+    this.loadMap();
 
     this.translateService.get('PROFILE_INFO_1').subscribe((value) => {
       this.PROFILE_INFO_1 = value;
@@ -62,5 +65,20 @@ export class ProfileViewPage {
     this.navCtrl.push('SignupPage');
   }
 
+  locationString:string;
+  mapUrl:any;
+  loadMap() {
+
+    let location = [];
+    if (this._user.city) location.push(this._user.city);
+    if (this._user.district) location.push(this._user.district);
+    if (this._user.country) location.push(this._user.country);
+
+    if (location.length > 0){
+      this.locationString = location.join('%2C');
+      this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://maps.google.com/maps?q='+this.locationString+'&t=&z=14&ie=UTF8&iwloc=&output=embed');
+    }
+    return;
+  }
 
 }
